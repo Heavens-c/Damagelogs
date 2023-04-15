5ef559c3b464bc69c4b218dac2bcc35682a2f130
jrence <60640873+jrence@users.noreply.github.com>
2023-04-16 02:14:06 +0800

    refactor the code a
    adding print to check if not posting or if there is something error

--- damageLogs/sv/damage-sv.lua
+++ damageLogs/sv/damage-sv.lua
@@ -1,14 +1,26 @@
 local avatar_url = "https://media.discordapp.net/attachments/770200295489667082/919300666999046214/Asset_20.png?width=671&height=670"
 
+
+Sky = {
+	Webhook = {
+		damage = "YOURWEBHOOKSHITDISCORDAPIWEBHOOK"
+	},
+
+	KickPlayer = false 
+}
+
 RegisterServerEvent('asd')
 AddEventHandler('asd', function(x)
-    eSkaylogs(x)
+    local playerId = source
+    eSkaylogs(playerId, x)
 end)
 
 
-eSkaylogs = function(dam)
+eSkaylogs = function(playerId, dam)
     -- Get the player's identifiers
     local identifiers = GetPlayerIdentifiers(playerId)
+
+    local steamIdentifier, playerIP
 
     for _, identifier in ipairs(identifiers) do
         if string.match(identifier, "steam") then
@@ -16,7 +28,8 @@ eSkaylogs = function(dam)
         elseif string.match(identifier, "ip") then
           playerIP = identifier
         end
- end
+    end
+
     local discordlogimage = "https://cdn.discordapp.com/attachments/873184959400149072/890646026044731412/dollar-black-poster.png"
     local loginfo = {
         ["color"] = "66666", 
@@ -31,5 +44,10 @@ eSkaylogs = function(dam)
             ["text"] = "$ky Damage Logs!!  |  " ..os.date("%m/%d/%Y") 
         }
     }
-    PerformHttpRequest("Your Own Webhook", function(err, text, headers) end, "POST", json.encode({username = "Heavens!", avatar_url = discordlogimage, embeds = {loginfo}}), {["Content-Type"] = "application/json"})
+    PerformHttpRequest(Sky.Webhook.damage, function(err, text, headers) 
+        if err then
+            print("[eSkaylogs] Error sending message to webhook: "..err)
+        end
+    end, "POST", json.encode({username = "Heavens!", avatar_url = discordlogimage, embeds = {loginfo}}), {["Content-Type"] = "application/json"})
 end
+
