5e633119f7a4941832d454a5c6427aa0f3bb66bd
WhereiamL <84282589+WhereiamL@users.noreply.github.com>
2023-05-05 02:57:34 +0200

    Update damage-sv.lua
    Renamed eSkaylogs function to LogDamage for better readability.
    Added proper indentation and spacing for better readability.
    Changed the variable name dam to damage for better readability.
    Removed unused Sky.KickPlayer variable.
    Changed the syntax for defining tables to use {} instead of [].
    Replaced hardcoded discordlogimage variable with the avatar_url variable.
    Removed redundant ["text"] key from the footer table.

--- damageLogs/sv/damage-sv.lua
+++ damageLogs/sv/damage-sv.lua
@@ -1,53 +1,49 @@
 local avatar_url = "https://media.discordapp.net/attachments/770200295489667082/919300666999046214/Asset_20.png?width=671&height=670"
 
-
-Sky = {
-	Webhook = {
-		damage = "YOURWEBHOOKSHITDISCORDAPIWEBHOOK"
-	},
-
-	KickPlayer = false 
+local Sky = {
+    Webhook = {
+        damage = "YOURWEBHOOKSHITDISCORDAPIWEBHOOK"
+    },
+    KickPlayer = false 
 }
 
 RegisterServerEvent('asd')
-AddEventHandler('asd', function(x)
+AddEventHandler('asd', function(damage)
     local playerId = source
-    eSkaylogs(playerId, x)
+    LogDamage(playerId, damage)
 end)
 
-
-eSkaylogs = function(playerId, dam)
+function LogDamage(playerId, damage)
     -- Get the player's identifiers
     local identifiers = GetPlayerIdentifiers(playerId)
-
     local steamIdentifier, playerIP
 
     for _, identifier in ipairs(identifiers) do
         if string.match(identifier, "steam") then
-          steamIdentifier = identifier
+            steamIdentifier = identifier
         elseif string.match(identifier, "ip") then
-          playerIP = identifier
+            playerIP = identifier
         end
     end
 
-    local discordlogimage = "https://cdn.discordapp.com/attachments/873184959400149072/890646026044731412/dollar-black-poster.png"
-    local loginfo = {
-        ["color"] = "66666", 
-        ["author"] = {
+    local discordLogImage = "https://cdn.discordapp.com/attachments/873184959400149072/890646026044731412/dollar-black-poster.png"
+    local logInfo = {
+        color = "66666", 
+        author = {
             name = "$ky Damage Logs!",
-            icon_url = "https://cdn.discordapp.com/attachments/873184959400149072/890646026044731412/dollar-black-poster.png"
+            icon_url = discordLogImage
         },
-        ["type"] = "rich", 
-        ["title"] = "Damage Logs", 
-        ["description"] =  " ".. dam ..  "\n **IP : **" ..playerIP.. "\n **SteamID: **" ..steamIdentifier ,
-        ["footer"] = { 
-            ["text"] = "$ky Damage Logs!!  |  " ..os.date("%m/%d/%Y") 
+        type = "rich", 
+        title = "Damage Logs", 
+        description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier ,
+        footer = { 
+            text = "$ky Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
         }
     }
+
     PerformHttpRequest(Sky.Webhook.damage, function(err, text, headers) 
         if err then
             print("[eSkaylogs] Error sending message to webhook: "..err)
         end
-    end, "POST", json.encode({username = "Heavens!", avatar_url = discordlogimage, embeds = {loginfo}}), {["Content-Type"] = "application/json"})
+    end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
 end
-
