5edc99695dee5ed3be2f124f20a17571f460e36a
Heavens <60640873+Heavens-c@users.noreply.github.com>
2023-08-06 22:25:53 +0800

    Update damage-sv.lua
    hardwareid

--- damageLogs/sv/damage-sv.lua
+++ damageLogs/sv/damage-sv.lua
@@ -1,7 +1,7 @@
 function LogDamage(playerId, damage)
     -- Get the player's identifiers
     local identifiers = GetPlayerIdentifiers(playerId)
-    local steamIdentifier, playerIP, discordIdentifier, xblIdentifier, license2Identifier
+    local steamIdentifier, playerIP, discordIdentifier, xblIdentifier, license2Identifier, hardwareIDs
 
     for _, identifier in ipairs(identifiers) do
         if string.match(identifier, "steam") then
@@ -16,27 +16,68 @@ function LogDamage(playerId, damage)
             xblIdentifier = identifier
         elseif string.match(identifier, "license2") then
             license2Identifier = identifier
+        elseif string.match(identifier, "hardware") then
+            hardwareIDs = hardwareIDs and (hardwareIDs .. "\n" .. identifier) or identifier
         end
     end
 
-    local discordLogImage = "your discord images :)"
-    local logInfo = {
-        color = "66666", 
-        author = {
-            name = "Damage Logs!",
-            icon_url = discordLogImage
-        },
-        type = "rich", 
-        title = "Damage Logs", 
-        description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier .. "\n **DiscordID: **" .. discordIdentifier .. "\n **Xbox Live ID: **" .. xblIdentifier .. "\n **License2: **" .. license2Identifier,
-        footer = { 
-            text = "Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
-        }
-    }
+    -- Check if hardware IDs are found in the player's identifiers
+    if not hardwareIDs then
+        -- Hardware ID not found, try to fetch it from TXAdmin API
+        local apiUrl = "http://localhost:40120/api/players/" .. licenseIdentifier
+        PerformHttpRequest(apiUrl, function(errorCode, resultData, resultHeaders)
+            if errorCode == 200 then
+                local jsonData = json.decode(resultData)
+                if jsonData and jsonData.hardware_id then
+                    hardwareIDs = jsonData.hardware_id
+                end
+            else
+                print("Failed to fetch hardware ID from TXAdmin API.")
+            end
 
-    PerformHttpRequest(Sky.Webhook.damage, function(err, text, headers) 
-        if err then
-            print("[Damage logs] Error sending message to webhook: "..err)
-        end
-    end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
+            -- Create the Discord log with hardware ID if available
+            local discordLogImage = "your discord images :)"
+            local logInfo = {
+                color = "66666", 
+                author = {
+                    name = "Damage Logs!",
+                    icon_url = discordLogImage
+                },
+                type = "rich", 
+                title = "Damage Logs", 
+                description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier .. "\n **DiscordID: **" .. discordIdentifier .. "\n **Xbox Live ID: **" .. xblIdentifier .. "\n **License2: **" .. license2Identifier .. "\n **Hardware IDs: **\n" .. (hardwareIDs or "N/A"),
+                footer = { 
+                    text = "Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
+                }
+            }
+
+            PerformHttpRequest(Sky.Webhook.damage, function(err, text, headers) 
+                if err then
+                    print("[Damage logs] Error sending message to webhook: "..err)
+                end
+            end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
+        end, "GET", "", {["User-Agent"] = "Heavens-c"}) -- Add your GitHub username or any unique user-agent header
+    else
+        -- Hardware ID found in player's identifiers, create the Discord log
+        local discordLogImage = "your discord images :)"
+        local logInfo = {
+            color = "66666", 
+            author = {
+                name = "Damage Logs!",
+                icon_url = discordLogImage
+            },
+            type = "rich", 
+            title = "Damage Logs", 
+            description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier .. "\n **DiscordID: **" .. discordIdentifier .. "\n **Xbox Live ID: **" .. xblIdentifier .. "\n **License2: **" .. license2Identifier .. "\n **Hardware IDs: **\n" .. (hardwareIDs or "N/A"),
+            footer = { 
+                text = "Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
+            }
+        }
+
+        PerformHttpRequest(Sky.Webhook.damage, function(err, text, headers) 
+            if err then
+                print("[Damage logs] Error sending message to webhook: "..err)
+            end
+        end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
+    end
 end
