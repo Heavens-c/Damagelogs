0e56efc39055fba16b2d781aa4e7f3452513d711
Heavens <60640873+Heavens-c@users.noreply.github.com>
2023-09-05 02:39:03 +0800

    Update fxmanifest.lua
    add esx dependencies

--- damageLogs/fxmanifest.lua
+++ damageLogs/fxmanifest.lua
@@ -5,6 +5,8 @@ author 'Heavens <Solbac1134>'
 description 'Simple Damage Logs with bone parts Credits on CFX natives'
 version '0.1.0'
 
+shared_script '@es_extended/imports.lua'
+
 
 client_scripts {
     'cl/*.lua',
