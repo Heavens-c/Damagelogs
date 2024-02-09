4d3e2ce00b78bcad4ac7169f1c8eb478f0afdf80
Heavens <60640873+Heavens-c@users.noreply.github.com>
2024-02-10 05:11:40 +0800

    Update damage-sv.lua refactor all my mistake ahaha
    double the function of logging one is for damage and bone  
    
    one is for impact only and getting rid of export of esx since i use fivem natives and lua so i consired working this on esx and qbcore? or qbox? or ox?

--- damageLogs/sv/damage-sv.lua
+++ damageLogs/sv/damage-sv.lua
@@ -1,88 +1,84 @@
-ESX = exports["es_extended"]:getSharedObject()
+local webhook = "Webhook" -- Define your webhook URL here
+local webhook2 = "Webhook" -- Define your webhook URL here2
+local discordLogImage = "https://cdn.discordapp.com/attachments/873184959400149072/890646026044731412/dollar-black-poster.png"
+
+--current todo is fetch player ip but not in plan because of TOS on my country privacy breach and steam maybe---
+
+RegisterServerEvent('damagebone')
+AddEventHandler('damagebone', function(damage)
+    local playerId = source
+    LogDamage(playerId, damage)
+end)
 
 
-local webhook = "YOUR_WEBHOOK_URL" -- Replace this with your actual webhook URL
+RegisterServerEvent('totaldamage')
+AddEventHandler('totaldamage', function(damage1)
+    local playerId = source
+    totaldamage(playerId, damage1)
+end)
+
 
 function LogDamage(playerId, damage)
     -- Get the player's identifiers
     local identifiers = GetPlayerIdentifiers(playerId)
-    local steamIdentifier, playerIP, discordIdentifier, xblIdentifier, license2Identifier, hardwareIDs
+    local steamIdentifier, playerIP
 
-    for _, identifier in ipairs(identifiers) do
-        if string.match(identifier, "steam") then
-            steamIdentifier = identifier
-        elseif string.match(identifier, "ip") then
-            playerIP = identifier
-        elseif string.match(identifier, "license") then
-            licenseIdentifier = identifier
-        elseif string.match(identifier, "discord") then
-            discordIdentifier = identifier
-        elseif string.match(identifier, "xbl") then
-            xblIdentifier = identifier
-        elseif string.match(identifier, "license2") then
-            license2Identifier = identifier
-        elseif string.match(identifier, "hardware") then
-            hardwareIDs = hardwareIDs and (hardwareIDs .. "\n" .. identifier) or identifier
-        end
-    end
-
-    -- Check if hardware IDs are found in the player's identifiers
-    if not hardwareIDs then
-        -- Hardware ID not found, try to fetch it from TXAdmin API
-        local apiUrl = "http://localhost:40120/api/players/" .. licenseIdentifier
-        PerformHttpRequest(apiUrl, function(errorCode, resultData, resultHeaders)
-            if errorCode == 200 then
-                local jsonData = json.decode(resultData)
-                if jsonData and jsonData.hardware_id then
-                    hardwareIDs = jsonData.hardware_id
-                end
-            else
-                print("Failed to fetch hardware ID from TXAdmin API.")
-            end
-
-            -- Create the Discord log with hardware ID if available
-            local discordLogImage = "your discord images :)"
-            local logInfo = {
-                color = "66666", 
-                author = {
-                    name = "Damage Logs!",
-                    icon_url = discordLogImage
-                },
-                type = "rich", 
-                title = "Damage Logs", 
-                description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier .. "\n **DiscordID: **" .. discordIdentifier .. "\n **Xbox Live ID: **" .. xblIdentifier .. "\n **License2: **" .. license2Identifier .. "\n **Hardware IDs: **\n" .. (hardwareIDs or "N/A"),
-                footer = { 
-                    text = "Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
-                }
-            }
-
-            PerformHttpRequest(webhook, function(err, text, headers) 
-                if err then
-                    print("[Damage logs] Error sending message to webhook: "..err)
-                end
-            end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
-        end, "GET", "", {["User-Agent"] = "Heavens-c"}) -- Add your GitHub username or any unique user-agent header
-    else
-        -- Hardware ID found in player's identifiers, create the Discord log
-        local discordLogImage = "your discord images :)"
-        local logInfo = {
-            color = "66666", 
-            author = {
-                name = "Damage Logs!",
-                icon_url = discordLogImage
-            },
-            type = "rich", 
-            title = "Damage Logs", 
-            description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier .. "\n **DiscordID: **" .. discordIdentifier .. "\n **Xbox Live ID: **" .. xblIdentifier .. "\n **License2: **" .. license2Identifier .. "\n **Hardware IDs: **\n" .. (hardwareIDs or "N/A"),
-            footer = { 
-                text = "Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
-            }
+   
+    local logInfo = {
+        color = "66666", 
+        author = {
+            name = "Damage Logs!",
+            icon_url = discordLogImage
+        },
+        type = "rich", 
+        title = "Damage Logs", 
+        description = damage .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier ,
+        footer = { 
+            text = "Damage Logs!!  |  " .. os.date("%m/%d/%Y") 
         }
-        
-        PerformHttpRequest(webhook, function(err, text, headers) 
-            if err then
-                print("[Damage logs] Error sending message to webhook: "..err)
-            end
-        end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
-    end
+    }
+
+    PerformHttpRequest(webhook, function(err, text, headers) 
+        if err then
+            print("[Damage logs] Error sending message to webhook: "..err)
+        end
+    end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
 end
+
+
+
+
+function totaldamage(playerId, damage1)
+    -- Get the player's identifiers
+    local identifiers = GetPlayerIdentifiers(playerId)
+    local steamIdentifier, playerIP
+
+   
+    local logInfo = {
+        color = "66666", 
+        author = {
+            name = "Damage Logs!",
+            icon_url = discordLogImage
+        },
+        type = "rich", 
+        title = "Damage Logs", 
+        description = damage1 .. "\n **IP : **" .. playerIP .. "\n **SteamID: **" .. steamIdentifier ,
+        footer = { 
+            text = "Damage Logs indicator!!  |  " .. os.date("%m/%d/%Y") 
+        }
+    }
+
+    PerformHttpRequest(webhook2, function(err, text, headers) 
+        if err then
+            print("[Damage logs] Error sending message to webhook: "..err)
+        end
+    end, "POST", json.encode({username = "Heavens!", avatar_url = avatar_url, embeds = {logInfo}}), {["Content-Type"] = "application/json"})
+end
+
+
+
+AddEventHandler('weaponDamageEvent', function(sender, data)
+    local damage = data.weaponDamage
+    local isKill = data.willKill
+   	TriggerClientEvent('damagelogs',sender, data.weaponDamage, sender,isKill)
+end)
